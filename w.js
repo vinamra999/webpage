bb139bcbce25d0a4983c4140494887350333eb88
Ritesh-Aldak <riteshaldak13022000@gmail.com>
2021-10-25 01:43:54 +0530

    fetching api and set all thing for fetching data

--- w.js
+++ w.js
@@ -1,19 +1,1 @@
 
-fetch("https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple"{
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
-		"x-rapidapi-key": "220a73f2e6msh0eaa76439d0b2b5p16b231jsna6f7a5b78fcb"
-	}
-}).then(response => {
-	const data = response.json();
-    console.log(data);
-})
-.then(data => {
-	console.log(data);
-	document.getElementById('para-output-1').innerText = 'data.'
-	
-})
-.catch(err => {
-	console.error(err);
-});
